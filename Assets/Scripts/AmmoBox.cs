0b6a910835d08a7cac7745948315af49a3d44a0d
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 <nabag123@newschool.edu>
2023-04-15 23:37:30 -0400

    Successful film pickup, however all objects disappear on first pickup

---
+++ Assets/Scripts/AmmoBox.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AmmoBox : MonoBehaviour
+{
+    public int ammoAmount = 10; // The amount of ammo the box contains
+
+    private bool collected = false; // Whether the box has already been collected
+
+    public AudioSource filmSound;
+    public TextMeshProUGUI filmText;
+    public GameObject Instruction;
+    public GameObject ThisTrigger;
+    public GameObject ObjectOnGround;
+    public bool Action = false;
+
+    private bool hasInteracted = false;
+
+    void Start()
+    {
+        Instruction.SetActive(false);
+        ThisTrigger.SetActive(true);
+        ObjectOnGround.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.transform.tag == "Player")
+        {
+            Instruction.SetActive(true);
+            Action = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        Instruction.SetActive(false);
+        Action = false;
+        ThisTrigger.SetActive(false);
+    }
+
+    public void CollectAmmo(AmmoSystem ammoSystem)
+    {
+        if (!collected)
+        {
+            ammoSystem.AddAmmo(ammoAmount);
+            collected = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ObjectOnGround.SetActive(false);
+        }
+
+        //if (Input.GetKeyDown(KeyCode.E) && !hasInteracted)
+        //{
+        //    if (ThisTrigger.Bounds.Contains(transform.position))
+        //    {
+        //        ThisTrigger.SetActive(false);
+        //        hasInteracted = true;
+        //    }
+        //}
+    }
+}
---
+++ Assets/Scripts/AmmoSystem.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class AmmoSystem : MonoBehaviour
+{
+    public GameObject Instruction;
+    //public GameObject ObjectOnGround;
+
+
+    public int maxAmmo = 50; // The maximum amount of ammo the player can carry
+    public int startingAmmo = 10; // The starting amount of ammo the player has
+    public float interactDistance = 2f; // The distance at which the player can interact with the ammo box
+    public KeyCode interactKey = KeyCode.E; // The key the player needs to hold to interact with the ammo box
+
+    public TextMeshProUGUI ammoText; // The UI text that displays the player's ammo count
+
+    private int currentAmmo; // The current amount of ammo the player has
+
+    private void Start()
+    {
+        Instruction.SetActive(false);
+        //ObjectOnGround.SetActive(true);
+
+        currentAmmo = startingAmmo;
+        UpdateAmmoText();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(interactKey))
+        {
+            Instruction.SetActive(false);
+            //ObjectOnGround.SetActive(false);
+
+
+            // Check if the player is within the collider of an ammo box
+            Collider[] colliders = Physics.OverlapSphere(transform.position, interactDistance);
+            foreach (Collider collider in colliders)
+            {
+                if (collider.CompareTag("AmmoBox"))
+                {
+                    // Get the AmmoBox component from the collider and call its CollectAmmo method
+                    AmmoBox ammoBox = collider.GetComponent<AmmoBox>();
+                    if (ammoBox != null)
+                    {
+                        ammoBox.CollectAmmo(this);
+                    }
+                }
+            }
+        }
+    }
+
+    public void AddAmmo(int amount)
+    {
+        currentAmmo = Mathf.Min(currentAmmo + amount, maxAmmo);
+        UpdateAmmoText();
+    }
+
+    public void UpdateAmmoText()
+    {
+        ammoText.text = currentAmmo + " / " + maxAmmo;
+    }
+}
