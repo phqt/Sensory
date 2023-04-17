71d17dd88351e5be501e511d895ed6d57f37e5a7
Isaac <isaactian.contact@gmail.com>
2023-04-16 21:41:18 -0400

    fixed film script POOOOG (hopefully)

--- Assets/Scripts/InstructionEnter.cs
+++ Assets/Scripts/InstructionEnter.cs
@@ -7,13 +7,15 @@ using Collider = UnityEngine.Collider;
 
 public class InstructionEnter : MonoBehaviour
 {
+    public int ammoAmount = 10;
 
     public GameObject Instruction;
     //public Collider ThisTrigger;
     public GameObject thisTrigger;
     public GameObject filmRoll;
-
-    public bool Action = false;
+    public AudioSource filmSound;
+    public bool Action;
+    public bool inRange;
 
 
     void Start()
@@ -21,31 +23,55 @@ public class InstructionEnter : MonoBehaviour
 
         Instruction.SetActive(false);
         this.GetComponent<BoxCollider>().enabled = true;
+        thisTrigger.SetActive(true);
     }
-
-
 
     void OnTriggerEnter(Collider other)
     {
-            Instruction.SetActive(true);
-            Action = true;
+        inRange = true;
     }
 
-    void OnTriggerExit(Collider collision)
+    void OnTriggerExit(Collider other)
+    {
+        inRange = false;
+        thisTrigger.SetActive(true);
+        //Instruction.SetActive(false);
+    }
+
+    public void CollectAmmo(AmmoSystem ammoSystem)
+    {
+        if (!Action)
+        {
+            ammoSystem.AddAmmo(ammoAmount);
+            Action = true;
+        }
+    }
+
+
+    void Update()
     {
         //Instruction.SetActive(false);
-        //Action = false;
-        //ThisTrigger.SetActive(false);
         this.GetComponent<BoxCollider>().enabled = true;
-        this.thisTrigger.SetActive(true);
-        Action = false;
+        thisTrigger.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.E) && inRange == true)
+        {
+
+            Instruction.SetActive(false);
+            filmRoll.SetActive(false);
+            Action = true;
+            filmSound.Play();
+            thisTrigger.SetActive(false);
+            this.GetComponent<BoxCollider>().enabled = false;
+        }
+        else
+        {
+
+            //this.GetComponent<BoxCollider>().enabled = true;
+            Action = false;
+            thisTrigger.SetActive(true);
+            this.GetComponent<BoxCollider>().enabled = true;
+        }
     }
 
-    void update()
-    {
-        Action = false;
-        Instruction.SetActive(false);
-        this.GetComponent<BoxCollider>().enabled = true;
-
-    }
 }
