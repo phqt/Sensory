c9eb782a9a987637d68216862bcf9e9c51adb392
Isaac <isaactian.contact@gmail.com>
2023-04-20 15:37:39 -0400

    restart ending scene, imported death vid

--- Assets/Scripts/CollidePlayer.cs
+++ Assets/Scripts/CollidePlayer.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 
 public class CollidePlayer : MonoBehaviour
 {
+    public float waitTime = 19f;
+    public float oneSec = 1f;
+
     void OnTriggerEnter(Collider c){
          if(c.gameObject.name == "Player")
             Debug.Log ("Player triggered");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);  
-            
-         
-     }
+        StartCoroutine(waitDeath());
+        SceneManager.LoadScene(4);
+    }
+    IEnumerator waitDeath()
+    {
+        yield return new WaitForSeconds(waitTime);
+        StartCoroutine(waitOne());
+
+    }
+    IEnumerator waitOne()
+    {
+        yield return new WaitForSeconds(oneSec);
+        SceneManager.LoadScene(2);
+    }
 }
+
---
+++ Assets/restartGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartGame : MonoBehaviour
+{
+    void Awake()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
