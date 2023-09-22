66ab1075fc09f175118c0568f746dbe57831f796
Isaac <isaactian.contact@gmail.com>
2023-09-22 19:41:21 -0400

    death anim

--- Assets/Scripts/ActivateEnemy.cs
+++ Assets/Scripts/ActivateEnemy.cs
@@ -8,6 +8,6 @@ public class ActivateEnemy : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
-        EnemyAi.isChasing = true;
+        EnemyDeath.isChasing = true;
     }
 }
--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyDeath : MonoBehaviour
 {
     public int EnemyHealth = 10;
     public GameObject TheEnemy;
-    public int StatusCheck;
+    //public int StatusCheck;
 
-    void DamageEnemy (int DamageAmount)
+    public GameObject EnemyGoal;
+    NavMeshAgent EnemyAgent;
+    public static bool isChasing;
+    public static bool isAlive;
+
+    private void Start()
+    {
+        EnemyAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void DamageEnemy(int DamageAmount)
     {
         EnemyHealth -= DamageAmount;
     }
@@ -16,10 +28,34 @@ public class EnemyDeath : MonoBehaviour
 
     void Update()
     {
-        if (EnemyHealth <= 0 && StatusCheck == 0)
+        if (isChasing == false)
         {
-            StatusCheck = 2;
-            TheEnemy.SetActive(false);
+            TheEnemy.GetComponent<Animator>().Play("Orc Idle");
+            isAlive = true;
+        }
+
+            if (isChasing == true && EnemyHealth >= 0 && isAlive == true)
+            {
+                
+                TheEnemy.GetComponent<Animator>().Play("Sad Walk");
+                EnemyAgent.SetDestination(EnemyGoal.transform.position);
+            }
+
+
+            if (EnemyHealth <= 0)
+            {
+            //StatusCheck = 2;
+                isAlive = false;
+                EnemyAgent.Stop();
+                TheEnemy.GetComponent<Animator>().Play("Stunned");
+                StartCoroutine(waitDeath());
+            }
+
+            IEnumerator waitDeath()
+            {
+                yield return new WaitForSeconds(3f);
+                TheEnemy.SetActive(false);
+            }
+
         }
     }
-}
